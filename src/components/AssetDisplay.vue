a266ba17c2a194802c7544784872c008d68b02bd
GP Gonzales <GerfelPhilip.Gonzales@microchip.com>
2025-07-23 14:27:07 +0800

    improved in/out/hold visibility

--- src/components/AssetDisplay.vue
+++ src/components/AssetDisplay.vue
@@ -4,103 +4,88 @@
 
     <b-row class="mt-4" v-if="this.checkState == 0">
       <b-col>
-        <b-table :items="items">
+        <b-table :items="items" :fields="['icon', 'name', 'value']" :row-class="rowClass" class="text-left">
           <template #head(icon)="">
             <span></span>
           </template>
           <template #cell(icon)="data">
             <b-icon :icon="data.value" />
           </template>
+          <template #cell(name)="datum">
+            <span v-if="datum.item.name === 'Asset Status'">
+                <b-badge variant="info" class="badge-big">{{ datum.item.name }}</b-badge> </span>
+            <span v-else>{{ datum.item.name }}</span>
+          </template>
+          <template #cell(value)="data">
+              <span v-if="data.item.value === 'IN'">
+                <b-badge variant="success" class="badge-big">{{ data.item.value }}</b-badge> </span>
+              <span v-else-if="data.item.value === 'OUT'">
+                <b-badge variant="primary" class="badge-big">{{ data.item.value }}</b-badge> </span>
+              <span v-else-if="data.item.value === 'HOLD'">
+                <b-badge variant="warning" class="badge-big">{{ data.item.value }}</b-badge> </span>
+            <span v-else>{{ data.item.value }}</span>
+          </template>
         </b-table>
         <img :src="this.asset.image" v-if="this.asset.image" style="max-width: 400px; height: auto;" />
       </b-col>
       <b-col>
 
-        <b-alert
-          show
-          variant="info"
-          v-if="this.asset.status_label.status_meta == 'deployed'"
-        >
+        <b-alert show variant="info" v-if="this.asset.status_label.status_meta == 'deployed'">
           <div class="text-center">
             <!-- Avatar Image -->
-            <img 
-              :src="user.avatar" 
-              v-if="user.avatar"
-              alt="Assigned User Avatar"
-              class="rounded-circle"
-              width="auto" height="200"
-            />
+            <img :src="user.avatar" v-if="user.avatar" alt="Assigned User Avatar" class="rounded-circle" width="auto"
+              height="200" />
             <!-- Assigned User Info -->
             <div class="ml-2">
               This asset is deployed to:<br />
               {{ this.asset.assigned_to.name }} ({{ this.asset.assigned_to.type }})
             </div>
-            
+
           </div>
         </b-alert>
-        
-        <b-alert
-          show
-          variant="danger"
-          v-else-if="this.asset.status_label.status_meta == 'undeployable'"
-        >
-          <b-icon-exclamation-circle-fill show variant="danger" style="width: 20px; height: 20px;" />    HOLD! This asset can not be deployed<br />
+
+        <b-alert show variant="danger" v-else-if="this.asset.status_label.status_meta == 'undeployable'">
+          <b-icon-exclamation-circle-fill show variant="danger" style="width: 20px; height: 20px;" /> HOLD! This asset
+          can not
+          be deployed<br />
           State: {{ this.asset.status_label.name }}
         </b-alert>
 
         <b-alert show variant="danger" v-else>
-          <b-icon-exclamation-circle-fill show variant="danger" style="width: 20px; height: 20px;" />    HOLD! This asset is not checked-out to anyone.
+          <b-icon-exclamation-circle-fill show variant="danger" style="width: 20px; height: 20px;" /> HOLD! This asset
+          is not
+          checked-out to anyone.
         </b-alert>
 
-        <b-alert variant="warning" show v-if="countdown > 0"
-          class="d-inline-block px-3 py-2"
-          style="width: auto; white-space: nowrap;"
-          >
-                Redirecting in {{ countdown }} seconds...
+        <b-alert variant="warning" show v-if="countdown > 0" class="d-inline-block px-3 py-2"
+          style="width: auto; white-space: nowrap;">
+          Redirecting in {{ countdown }} seconds...
         </b-alert>
 
-        <KeyboardReader
-          @read="(resp) => onKeyboardRead(resp)"
-          @startReading="$emit('startScan')"
-        />
+        <KeyboardReader @read="(resp) => onKeyboardRead(resp)" @startReading="$emit('startScan')" />
         <div>
-        <Button
-          variant="primary"
-          @click="() => assetOUT()"
-          class="ml-2"
-          v-if="
+          <Button variant="primary" @click="() => assetOUT()" class="ml-2" v-if="
             this.asset.custom_fields['Asset Status'].value == 'IN' &&
             this.asset.custom_fields['Asset Status'].value != 'HOLD'
-          "
-        >
-          <b-icon-upc-scan /> Scan IN/OUT
-        </Button>
+          ">
+            <b-icon-upc-scan /> Scan IN/OUT
+          </Button>
 
-        <Button
-          variant="primary"
-          @click="() => assetIN()"
-          class="ml-2"
-          v-if="
+          <Button variant="primary" @click="() => assetIN()" class="ml-2" v-if="
             this.asset.custom_fields['Asset Status'].value == 'OUT' &&
             this.asset.custom_fields['Asset Status'].value != 'HOLD'
-          "
-        >
-        <b-icon-upc-scan /> Scan IN/OUT
-        </Button>
+          ">
+            <b-icon-upc-scan /> Scan IN/OUT
+          </Button>
 
-        <Button
-          variant="warning"
-          @click="() => hold()"
-          class="ml-2"
-          v-if="
+          <Button variant="warning" @click="() => hold()" class="ml-2" v-if="
             this.asset.custom_fields['Asset Status'].value == 'OUT' ||
             this.asset.custom_fields['Asset Status'].value == 'IN' &&
             this.asset.custom_fields['Asset Status'].value != 'HOLD'
-          "
-        >
-        <b-icon-upc-scan /> Scan HOLD
-        </Button>
-      </div>
+          ">
+            <b-icon-upc-scan /> Scan HOLD
+          </Button>
+        </div>
       </b-col>
 
     </b-row>
@@ -121,10 +106,7 @@
           </h5>
         </div>
         <div v-if="this.checkState == 4">
-          <b-icon-exclamation-octagon
-            variant="danger"
-            class="icon-big mt-4 mb-4"
-          />
+          <b-icon-exclamation-octagon variant="danger" class="icon-big mt-4 mb-4" />
           <h5>Please put the item back!</h5>
         </div>
         <div v-if="this.checkState == 5">
@@ -141,7 +123,7 @@ import Button from "./Button.vue";
 import KeyboardReader from "@/components/KeyboardReader.vue";
 
 export default {
-  components: {Button, KeyboardReader},
+  components: { Button, KeyboardReader },
   name: "AssetDisplay",
   data: () => ({
     checkState: 0, // 0: init, 1: loading, 2: success assetOUT, 3: success assetIN, 4: error, 5: warning hold;
@@ -154,8 +136,8 @@ export default {
     countdown: 10,
     countdownInterval: null,
   }),
-  mounted: function() {
-    if (this.asset.status_label.status_meta == 'deployed'){
+  mounted: function () {
+    if (this.asset.status_label.status_meta == 'deployed') {
       this.getUserAvatar(this.asset.assigned_to.id);
     }
 
@@ -168,13 +150,13 @@ export default {
         this.$router.push("/scan"); // Reset checkState after countdown
       }
     }, 1000);
-    
+
   },
   computed: {
     items: function () {
       let a = [
         { icon: "hdd", name: "Model number", value: this.asset.model_number },
-        { icon: "tag", name: "Asset tag", value: this.asset.asset_tag},
+        { icon: "tag", name: "Asset tag", value: this.asset.asset_tag },
         { icon: "key", name: "Serial number", value: this.asset.serial },
         {
           icon: "map",
@@ -199,6 +181,10 @@ export default {
     }
   },
   methods: {
+    rowClass(item, type) {
+      if (!item || type !== 'row') return '';
+      return item.name === 'Asset Status' ? 'highlight-row' : '';
+    },
     onKeyboardRead: function (input) {
       const val = input;
       if (val === 'out') {
@@ -284,4 +270,9 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.badge-big {
+  font-size: 1.5rem !important;
+  padding: 0.25rem 2rem !important;
+}
+</style>
